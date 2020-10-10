de3bc220eac7b2e93e792381746454c1f6389f17
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-10-10 22:43:55 +0100

    added emailJs

---
+++ js/sendemail.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'gmail';
+   const templateID = 'contact_form';
+
+   emailjs.send(serviceID, templateID, {
+    "from_name": form.name.value,
+    "from_email": form.email.value,
+    "message": form.message.value,
+   })
+    .then(() => {
+      btn.value = 'Send Message';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Message';
+      alert(JSON.stringify(err));
+    });
+});
