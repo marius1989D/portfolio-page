4ac1af95862abb30c3283a0b9e2dd4b9da13d06d
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-10-11 16:15:47 +0100

    added changing text for the headline

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+var text = ["self-taught", "commited", "curious"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 3000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
